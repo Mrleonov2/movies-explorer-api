a99f9dcbd8226a2b08a8310813637d650bcd4089
Mrleonov2 <mikhail-leonov2002@yandex.ru>
2022-11-23 18:46:03 +0700

    add corsOpt

--- utils/corsOption.js
+++ utils/corsOption.js
@@ -1,6 +1,12 @@
 const corsOption = {
   origin: [
+    'http://api.explorer.leonov.nomoredomains.club',
+    'https://api.explorer.leonov.nomoredomains.club',
+    'http://explorer.leonov.nomoredomains.club',
+    'https://explorer.leonov.nomoredomains.club',
     'http:/localhost:3000',
+    'https://api.explorer.leonov.nomoredomains.club/signup',
+    'https://api.explorer.leonov.nomoredomains.club/signin',
   ],
   methods: ['GET', 'PUT', 'POST', 'PATCH', 'DELETE', 'HEAD'],
   credentials: true,
