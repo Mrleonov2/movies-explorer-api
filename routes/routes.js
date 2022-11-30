a4bff84928218c740c56c51da70ce608760e6205
Mrleonov2 <mikhail-leonov2002@yandex.ru>
2022-12-01 05:13:16 +0700

    valid emailV2

--- routes/routes.js
+++ routes/routes.js
@@ -5,13 +5,14 @@ const usersRouter = require('./users');
 const moviesRouter = require('./movies');
 const NotFoundError = require('../errors/NotFoundError');
 const { createUser, login } = require('../controllers/users');
+const { emailValid } = require('../utils/constant');
 
 routes.post(
   '/signup',
   celebrate({
     body: Joi.object().keys({
       name: Joi.string().min(2).max(30).required(),
-      email: Joi.string().email().required(),
+      email: Joi.string().pattern(new RegExp(emailValid)).required(),
       password: Joi.string().required(),
     }),
   }),
@@ -21,7 +22,7 @@ routes.post(
   '/signin',
   celebrate({
     body: Joi.object().keys({
-      email: Joi.string().email().required(),
+      email: Joi.string().pattern(new RegExp(emailValid)).required(),
       password: Joi.string().required(),
     }),
   }),
